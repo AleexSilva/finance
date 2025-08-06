2e08bbc2a3a172b6f1e0094a9367fb223c313beb
AleexSilva <silva.alexis94@gmail.com>
2025-08-06 20:41:43 -0300

    random

--- intro.py
+++ intro.py
@@ -20,3 +20,13 @@ a = np.array([[0,2,3],[1,4,6]])
 a.shape
 
 
+# random to run simulations, beautiful for examing hipotetical scenarios. Sometimes, the real data is not available or not enough.
+
+prob = np.random.rand(1)
+print(prob)
+
+number = random.randint(1,10)
+print(number)
+
+np.random.randint(1,10,(4,6))
+
