cbefa300ae8581632d1aaa4307ad025d333488ee
AleexSilva <silva.alexis94@gmail.com>
2025-08-06 20:40:46 -0300

    import libraries

--- intro.py
+++ intro.py
@@ -1,3 +1,22 @@
+## Generating Random Numbers 
+# For simulations and examing hipotetycal scenarios
+# 
+
+
 import pandas as pd
+import numpy as np
+import math # For mathematical operations
+import random # For generating random numbers
+import statsmodels # For descriptive statistics, regression and time series 
+import pandas_datareader # For downloading stock data
+import matplotlib.pyplot as plt # For data visualization
+import seaborn as sns # For statistical visualization
+import scipy # For scientific computing
+
+
+# Arrays: All the data inside must be the same datatype
+
+a = np.array([[0,2,3],[1,4,6]])
+a.shape
 
 
